78583b4a0440c2619945ccba0cdec3da1b56f4b1
HenrikCheng <henrikcheng@live.se>
2023-05-17 14:42:00 +0200

    same as before

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,16 @@
+// import { authOptions } from " /server/auth";
 import NextAuth from "next-auth";
-import { authOptions } from " /server/auth";
+import DiscordProvider from "next-auth/providers/discord";
 
-export default NextAuth(authOptions);
+const scopes = ["identify"].join(" ");
+
+export default NextAuth({
+  // ...authOptions,
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID as string,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET as string,
+      authorization: { params: { scope: scopes } },
+    }),
+  ],
+});
