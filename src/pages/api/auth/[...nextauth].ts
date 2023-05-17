6c33d5a56b22b5dc26d5b7812e4fa45599e4063f
HenrikCheng <henrikcheng@live.se>
2023-05-17 14:22:11 +0200

    enable authoptions

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,11 @@
+import { authOptions } from " /server/auth";
 import NextAuth from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
-// import { authOptions } from " /server/auth";
 
 const scopes = ["identify"].join(" ");
 
 export default NextAuth({
-  // ...authOptions,
+  ...authOptions,
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID as string,
